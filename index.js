6f15d971c72548f7a08aaeaa3774c389ff0ceeec
anhlq <quoc.anh@your.rentals>
2020-02-27 13:14:21 +0700

    refresh

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "rules": {
+    "newline-per-chained-call": 0,
+    "no-underscore-dangle": 0,
+    "no-console": 2,
+    "no-param-reassign": 1,
+    "arrow-parens": 0,
+    "no-use-before-define": 0
+  },
+  "env": {
+    "node": true,
+    "mocha": true
+  }
+}

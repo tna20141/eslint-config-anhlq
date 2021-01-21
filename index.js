3db71a558b08611dfeb0b9843e6993bd1a22bcca
Anh Le <anhlq2110@gmail.com>
2021-01-21 11:36:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ module.exports = {
     "newline-per-chained-call": 0,
     "no-underscore-dangle": 0,
     "no-console": 2,
-    "no-param-reassign": 1,
+    "no-param-reassign": 2,
+    "prefer-destructuring": 0,
     "arrow-parens": 0,
+    "prefer-promise-reject-errors": 0,
     "no-use-before-define": 0
   },
   "env": {
